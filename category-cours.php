f2ed9c499ead1960239c720e072bedc575aa9e1d
Nejma <nejmahamadi@hotmail.com>
2019-02-28 18:33:03 -0500

    premier commit

--- category-cours.php
+++ category-cours.php
@@ -14,15 +14,10 @@ get_header();
 jjjjjjjjjjjjjjjjj - category-cours.php - jjjjjjjjjjjjjjjjjjj
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+		<div class="wrapper">
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
-			</header><!-- .page-header -->
-
+		
+		
 			<?php
 			// Start the Loop.
 			while ( have_posts() ) :
@@ -41,12 +36,15 @@ jjjjjjjjjjjjjjjjj - category-cours.php - jjjjjjjjjjjjjjjjjjj
 			// Previous/next page navigation.
 			twentynineteen_the_posts_navigation();
 
+
+
 			// If no content, include the "No posts found" template.
 		else :
 			get_template_part( 'template-parts/content/content', 'none' );
 
 		endif;
 		?>
+		</div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
